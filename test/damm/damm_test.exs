05674714b4dd79d4e32bde708cb1b8916f4276ce
Tomoka64 <y.tomoka64@gmail.com>
2020-04-07 20:31:19 +0900

    add damm algorithm

---
+++ test/damm/damm_test.exs
@@ -0,0 +1,25 @@
+defmodule Checkdigit.DammTest do
+    use ExUnit.Case
+
+    alias Checkdigit.{Damm}
+
+    test "Damm: regular 1" do
+      assert Damm.verify("224564332323") == true
+    end
+
+    test "Damm: regular 2" do
+      assert Damm.verify("543525432346") == true
+    end
+
+    test "Damm: regular 3" do
+      assert Damm.verify("37") == true
+    end
+
+    test "Damm: irregular 1" do
+      assert Damm.verify("835323233227") == false
+    end
+
+    test "Damm: irregular 2" do
+      assert Damm.verify("") == false
+    end
+end
