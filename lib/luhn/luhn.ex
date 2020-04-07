017a38eed9980a27b082c970b0c8c15be6cfed0f
Tomoka64 <y.tomoka64@gmail.com>
2020-04-07 19:35:13 +0900

    add luhn algorithm

---
+++ lib/luhn/luhn.ex
@@ -0,0 +1,39 @@
+defmodule Checkdigit.Luhn do
+  @spec verify(String.t()) :: boolean
+  def verify(code) when code == "", do: false
+
+  def verify(code) do
+    check_digit = String.at(code, String.length(code)-1) |> String.to_integer
+    generated = String.slice(code, 0..-2) |> generate
+    generated == check_digit
+  end
+
+  def generate(seed) when seed == "", do: 0
+
+  def generate(seed) do
+    parity = rem(String.length(seed) + 1, 2)
+    len = String.length(seed)
+    result = generate_helper(0, seed, parity, len) * 9 |> rem(10)
+    result
+  end
+
+  defp generate_helper(index, _, _, len) when index == len, do: 0
+
+  defp generate_helper(index, seed, parity, len) do
+    curr = String.at(seed, index) |> String.to_integer
+    modified =
+      if rem(index, 2) == parity do
+        ret =
+          case curr * 2 do
+            x when x > 9 -> x - 9
+            x -> x
+          end
+        ret
+      else
+        curr
+      end
+    next = modified + generate_helper(index+1, seed, parity, len)
+    next
+  end
+end
+
---
+++ test/luhn/luhn_test.exs
@@ -0,0 +1,25 @@
+defmodule Checkdigit.LuhnTest do
+  use ExUnit.Case
+
+  alias Checkdigit.{Luhn}
+
+  test "Luhn: regular 1" do
+    assert Luhn.verify("4242424242424242") == true
+  end
+
+  test "Luhn: regular 2" do
+    assert Luhn.verify("5105105105105100") == true
+  end
+
+  test "Luhn: regular 3" do
+    assert Luhn.verify("34") == true
+  end
+
+  test "Luhn: irregular 1" do
+    assert Luhn.verify("510510510510511") == false
+  end
+
+  test "Luhn: irregular 2" do
+    assert Luhn.verify("") == false
+  end
+end
